486e34611440748ec056607b587b3cb16384a490
MbPro13_chudikO <oleg.chudnovskiy.gmail.com>
2023-01-27 19:33:57 +0200

    task02 complate

---
+++ src/main/java/org/itstep/task02/City.java
@@ -0,0 +1,21 @@
+package org.itstep.task02;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class City {
+
+    private String name;
+    private String region;
+    private String country;
+    private int inhabitants;
+    private String index;
+    private String code;
+}
